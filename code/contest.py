d0146a459568e3d177c68f34997e815536a5b363
jumagaliev_samat <jumagaliev.samat@yandex.ru>
2024-07-06 10:21:40 +0300

    0.0.0.1

---
+++ code/contest.py
@@ -0,0 +1,9 @@
+from video_recorder import Record
+def record_video(request):
+    if request.config.getoption('--record_vid') == 'true':
+        recorder = Record(driver, file_name=f'{file_path("videos")}/{test_name}_video')
+        recorder.start()
+        yield
+        recorder.stop()
+    else:
+        yield
